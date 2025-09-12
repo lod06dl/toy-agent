c8a42da6362bcfd764489abe112e0f1a2150a014
livio <livio@uber.com>
2025-09-12 10:19:40 -0700

    agent can receive verbose option

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ def main():
 
     response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
     print(response.text)
-    print(response.usage_metadata.prompt_token_count)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    if (len(sys.argv) > 2):
+        if (sys.argv[2] == "--verbose"):
+            print(f"User prompt: {prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
